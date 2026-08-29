5561f163b8b3fc1640cb10ef45e3e36324144e2f
farukh <xfarukh.3@gmail.com>
2026-08-30 00:35:12 +0530

    Patient-serivce / Model/Patient.java created

---
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,37 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NonNull
+    private String address;
+
+    @NonNull
+    private LocalDate dateOfBirth;
+
+    @NonNull
+    private LocalDate registerDate;
+
+
+}
