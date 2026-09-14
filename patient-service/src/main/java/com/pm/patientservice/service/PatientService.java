de8e89d1ad01fc870215b1e32fc010c5a245671d
farukh <xfarukh.3@gmail.com>
2026-09-14 23:22:42 +0530

    create UPDATE endpoint,in dto created validators then CreatePatientValidationGroup and then in RequestDTO include registerDate into this group, created a method existBYEmailAndIdNot to check for duplicate email then only update , created PatientNotFoundException to check whether update by id have Patient or not

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -3,13 +3,19 @@ package com.pm.patientservice.service;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
+import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import jakarta.validation.groups.Default;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -39,6 +45,25 @@ public class PatientService {
         Patient newPatient=patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id,@RequestBody PatientRequestDTO patientRequestDTO){
+
+        Patient patient=patientRepository.findById(id).orElseThrow(()->new PatientNotFoundException("Patient not found with ID: " + id));
+
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(),id)){
+            throw new EmailAlreadyExistsException("A patient with this email " + "already exist " + patientRequestDTO.getEmail());
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        //patient.setRegisterDate(LocalDate.parse(patientRequestDTO.getRegisterDate()));
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatedPatient = patientRepository.save(patient);
+
+        return PatientMapper.toDTO(updatedPatient);
     }
 
 }
