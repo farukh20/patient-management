8cc46d9dd14b19dfb44d79edd6ad17b9c6093628
farukh <xfarukh.3@gmail.com>
2026-09-15 00:39:51 +0530

    delete void endpoint added

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -66,4 +66,7 @@ public class PatientService {
         return PatientMapper.toDTO(updatedPatient);
     }
 
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 }
